e526c939123ec72e33b5d9221fe4b59183e6b9a8
harshitha462000 <harshitha462000@gmail.com>
2024-01-16 10:25:41 +0530

    hello

---
+++ arrays.js
@@ -0,0 +1,36 @@
+// var a = 12;
+// if(78)
+// {
+//     console.log(a);
+//     var a = 78;
+//     console.log(a);
+// }
+// console.log(a);
+
+// var a = 12;
+// function test()
+// {
+//     var a = 45;
+//     console.log(a);
+// }
+// console.log(a);
+// test();
+// console.log(a);
+
+
+// var a = 12;
+// if(0)
+// {
+//     console.log(a);
+//     let a = 67;
+//     console.log((a));
+// }
+// console.log(a);
+
+//Arrays
+
+var arr = [1,2,3,4];
+for(let i =  0; i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
